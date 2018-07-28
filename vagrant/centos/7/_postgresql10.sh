779b477176be853710907e071ca2bb0fdab4ae53
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2018-07-28 23:54:45 +0900

    postgresql

--- vagrant/centos/7/_postgresql10.sh
+++ vagrant/centos/7/_postgresql10.sh
@@ -7,8 +7,8 @@ echo -------------------------------------------------
 # リポジトリ追加
 yum -y install https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-redhat10-10-2.noarch.rpm
 
-# PostgreSQL9.6インストール
-yum -y install postgresql10-server postgresql10-devel postgresql10-contrib
+# PostgreSQL10.0インストール
+yum -y install postgresql10-server postgresql10-devel
 
 # 初期設定
 /usr/pgsql-10/bin/postgresql-10-setup initdb
@@ -24,3 +24,6 @@ cp $PROVISION/conf/postgresql10/pg_hba.conf /var/lib/pgsql/10/data/pg_hba.conf
 # PostgreSQL起動&有効化
 systemctl start postgresql-10
 systemctl enable postgresql-10
+
+# vagrantユーザーの作成
+sudo -u postgres psql -c "CREATE ROLE vagrant WITH LOGIN SUPERUSER;"
--- vagrant/centos/7/_postgresql9.6.sh
+++ vagrant/centos/7/_postgresql9.6.sh
@@ -8,7 +8,7 @@ echo -------------------------------------------------
 yum -y install https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7.4-x86_64/pgdg-centos96-9.6-3.noarch.rpm
 
 # PostgreSQL9.6インストール
-yum -y install postgresql96-server postgresql96-devel postgresql96-contrib
+yum -y install postgresql96-server postgresql96-devel
 
 # 初期設定
 /usr/pgsql-9.6/bin/postgresql96-setup initdb
@@ -24,3 +24,6 @@ cp $PROVISION/conf/postgresql9.6/pg_hba.conf /var/lib/pgsql/9.6/data/pg_hba.conf
 # PostgreSQL起動&有効化
 systemctl start postgresql-9.6
 systemctl enable postgresql-9.6
+
+# vagrantユーザーの作成
+sudo -u postgres psql -c "CREATE ROLE vagrant WITH LOGIN SUPERUSER;"
